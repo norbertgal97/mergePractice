94685b1cd05f3ca40444970c0cfe89bee9baf51e
Norbert Gal <norbert.gal97@gmail.com>
2019-08-15 16:09:00 +0200

    1.

---
+++ mergePractice/mergePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mergePractice
+//
+//  Created by Norbert Gal on 2019. 08. 15..
+//  Copyright © 2019. AutSoft Zrt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
